213b2c3621fc51c843dd5722ac29d3b88f292dfa
Alberto Torralba-Torregrosa <alberto.torralba@uv.es>
2024-02-22 17:32:14 +0100

    Added main functions

--- src/PAUS_cutout/coadds.py
+++ src/PAUS_cutout/coadds.py
@@ -1,6 +1,11 @@
+import os
+import sys
+import shutil
+import glob
+
 import pandas as pd
 import sqlalchemy as sqla
-import os
+import numpy as np
 
 from astropy.io import fits
 from astropy.nddata import Cutout2D
@@ -9,7 +14,8 @@ from astropy.wcs import WCS
 import astropy.units as u
 from astropy.nddata.utils import NoOverlapError
 
-import numpy as np
+from .coadds import *
+
 
 def get_images_info(RA, DEC, square_size, NB_wavelength,
                     fully_contained_only=False):
@@ -97,3 +103,77 @@ def crop_images(df, RA, DEC, cutout_square_size, savepath,
             excluded_images.append(i)
     
     return excluded_images
+
+
+def rm_tmp_dir(func):
+    def inner(*args):
+        tmp_files_dir = args[-1]
+        # Never set exist_ok=True or very bad things could happen !!
+        os.makedirs(tmp_files_dir, exist_ok=False)
+        
+        try:
+            func(*args)
+        finally:
+            shutil.rmtree(tmp_files_dir)
+            raise
+        
+    return inner
+
+@rm_tmp_dir
+def generate_coadded_cutouts(RA_Arr, DEC_Arr, ID_Arr, square_size,
+                             NB_wav_Arr, tmp_files_dir='tmp_files_cutouts',
+                             save_coadds_dir='out_cutouts'):
+    for NB_wav in NB_wav_Arr:
+        save_coadds_to = f'{save_coadds_dir}/NB{int(NB_wav)}'
+        os.makedirs(save_coadds_to, exist_ok=True)
+        
+        for RA, DEC, ID in zip(RA_Arr, DEC_Arr, ID_Arr):
+            print(f'\n\n {RA=}, {DEC=}, {ID=}\n')
+            df = get_images_info(RA, DEC, square_size, NB_wav)
+
+            # Copy images to temporary directory
+            copy_images_to_home(df, tmp_files_dir)
+
+            # Crop images
+            excluded_images = crop_images(df, RA, DEC,cutout_square_size,
+                                          tmp_files_dir)
+            crop_images(df, RA, DEC, cutout_square_size, tmp_files_dir,
+                        suffix='.weight')
+            
+            # Remove excluded images from list
+            if len(excluded_images) > 0:
+                print(excluded_images)
+                with open(f'{tmp_files_dir}/img_list.txt', 'r') as file:
+                    img_list_lines = file.readlines()
+                    
+                with open(f'{tmp_files_dir}/img_list.txt', 'w') as file:
+                    for i, line in enumerate(img_list_lines):
+                        if i not in excluded_images:
+                            file.write(line)
+            ### Generate new Swarp config file from template
+            config_template = '/nfs/pic.es/user/a/atorralb/PAUS_cutouts/config.swarp'
+            with open(config_template, 'r') as file:
+                filedata = file.read()
+                
+            out_filename = f'{save_coadds_to}/coadd_cutout_{ID}_NB{NB_wav}.fits'
+            filedata = filedata.replace('coadd.fits', out_filename)
+            out_filename_w = f'{save_coadds_to}/coadd_cutout_{ID}_NB{NB_wav}.weight.fits'
+            filedata = filedata.replace('coadd.weight.fits', out_filename_w)
+           
+            with open(f'{tmp_files_dir}/config.swarp', 'w') as file:
+                file.write(filedata)
+
+            # Run swarp
+            img_list_path = f'{tmp_files_dir}/img_list.txt'
+            config_file_path = f'{tmp_files_dir}/config.swarp'
+            swarp_path = '/data/astro/software/centos7/swarp/2.41.5/bin/swarp'
+
+            os_out = os.system(f'{swarp_path} @{img_list_path} -c {config_file_path}')
+            if os_out != 0:
+                raise Exception(f'Error {os_out}')
+                
+            # Delete tmp files for this coadd
+            tmp_files = glob.glob(f'{tmp_files_dir}/*')
+            for f in tmp_files:
+                os.remove(f)
+                
